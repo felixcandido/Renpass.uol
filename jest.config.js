cb7d4e7e95aaeeac5ff991420223ca83bd09b21c
Félix Cândido <netocandido007@gmail.com>
2022-06-23 13:47:06 -0300

    refactor: remove useless flag in script dev and create .env.example

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   bail: true,
   clearMocks: true,
   collectCoverage: true,
-  // cnnpollectCoverageFrom: ["src/**", "!src/models"],
+  collectCoverageFrom: ['src/**', '!src/models'],
   coverageProvider: 'v8',
   testMatch: [
     '**/__tests__/**/*.test.js?(x)',
